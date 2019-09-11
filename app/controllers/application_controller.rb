d53ea21f5a4c98036ea4105394c34ae4f00e151c
Sara Harvey <saraharveynyc@gmail.com>
2019-09-11 17:52:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,5 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    
-    
+    redirect to 
 end
