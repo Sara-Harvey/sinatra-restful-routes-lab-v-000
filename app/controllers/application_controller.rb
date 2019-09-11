56286d5c8dd5eac8c08d36cd7d4bd8f6d4da9e7a
Sara Harvey <saraharveynyc@gmail.com>
2019-09-11 17:55:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,10 @@ class ApplicationController < Sinatra::Base
   
   get '/recipes/:id/edit' do 
     @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+    
+    @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name] 
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
